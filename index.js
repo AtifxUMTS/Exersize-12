25b07ef155fb25fffb0c910b108d7d15c9f3de78
AtifxUMTS <atifalishahnaqvi3345@gmail.com>
2023-10-01 09:02:16 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+// How to use array in typescript
+// Names: store the names of a few of your friends in a array.
+let names = ['Atif', 'Ali', 'Ayaz', 'Wajid'];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
---
+++ index.ts
@@ -0,0 +1,11 @@
+// How to use array in typescript
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let names: string[]=['Atif', 'Ali', 'Ayaz','Wajid']
+
+// Loop through each name in the array.
+
+for (let name of names) {
+    // Print a personilize message for each friend 
+console.log(``)
+}
